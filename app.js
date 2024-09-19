45d6db4d340cd29b22dcffe8dc596c512fc8272e
minhtuyen <nguyenminhtuyenkt98@gmail.com>
2024-09-19 20:49:51 +0700

    Init UI pagination

--- app.js
+++ app.js
@@ -1,84 +1,60 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const themeRouter = require('./routes/theme');
-const systemConfig = require('./config/system');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const themeRouter = require("./routes/theme");
+const systemConfig = require("./config/system");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('component', path.join(__dirname, 'component'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("component", path.join(__dirname, "component"));
+app.set("view engine", "ejs");
 
 // app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.locals.systemConfig = systemConfig.prefixTheme;
 app.locals.lala = "lalala";
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 app.use(`/${systemConfig.prefixTheme}`, themeRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-main().catch(err => console.log(err));
-
-// const modelList = require("./schemas/list")
-
+main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://nguyenminhtuyenkt98:19981998T@dbnodejs.oqvydox.mongodb.net/dataTranningNodej');
+  await mongoose.connect(
+    "mongodb+srv://nguyenminhtuyenkt98:19981998T@dbnodejs.oqvydox.mongodb.net/dataTranningNodej"
+  );
   console.log("connect thành công");
-//   const list = new Schema({
-//     id: Number,
-//     name: String,
-//     status: String
-//   });
-  
-// const modelList = mongoose.model('Tuyen', list);
-//    const people = await modelList.find({});
-    
-//     console.log("people:", people);
-//   const list = new Schema({
-//     id: Number,
-//     name: String,
-//     status: String
-//   });
-  
-// const modelList = mongoose.model('list', list);
-
-// module.export = modelList;
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
-
-
-
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('page/error/error');
+  res.render("page/error/error");
 });
 
 module.exports = app;
